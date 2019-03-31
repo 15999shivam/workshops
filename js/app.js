4ff231f04609dad676ffe4ec0aae9268ea5f8548
Shivam <shivamgarg260@gmail.com>
2019-03-31 18:38:33 +0530

    Landing With text and improved Scroll Down Button

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 5,
+        "value": 7,
         "density": {
           "enable": true,
           "value_area": 800
